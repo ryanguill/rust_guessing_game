dc2f3cb28be842f6d618c5c6c54773195373083e
Ryan Guill <ryanguill@gmail.com>
2019-06-02 08:57:34 -0500

    working!

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    const MAX_TRIES: i8 = 7;
+    let mut tries = 0;
+    let mut lower_bound = 1;
+    let mut upper_bound = 100;
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+
+    println!("I'm thinking of a number between 1 and 100. Can you guess what it is?");
+
+    loop {
+        if tries >= MAX_TRIES {
+            println!("Out of tries :(");
+            break;
+        } else {
+            let tries_remaining = MAX_TRIES - tries;
+            println!("You have {} {} remaining.", tries_remaining, if tries_remaining == 1 { "try" } else { "tries" });
+            println!("Please input your guess. Its a number between {} and {}", lower_bound, upper_bound);
+            tries += 1;
+        }
+
+        let mut guess = String::new();
+
+        // not sure how to work with this as a result, also not sure how to make this fail to try things.
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        //println!("you guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                lower_bound = guess;
+                println!("{} is too small!", guess)
+            }
+            Ordering::Greater => {
+                upper_bound = guess;
+                println!("{} is too big!", guess)
+            }
+            Ordering::Equal => {
+                println!("You win! and it only took you {} {}", tries, if tries == 1 { "try" } else { "tries" });
+                break;
+            }
+        }
+    }
+}
